b972198b8dea555b291e65251b319cdd7e337edd
FilipPersonalRepo <151617301+FilipPersonalRepo@users.noreply.github.com>
2024-05-03 13:57:27 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,16 +1,25 @@
+import json
 import yaml
 
 # Read parameters from input.yml
 with open('input.yml', 'r') as file:
     params = yaml.safe_load(file)
-print("CHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEECK")
-print(params)
 
 # Replace placeholders in run.json with parameters
 with open('run.json', 'r') as file:
     data = json.load(file)
-    for key, value in params.items():
-        data[key] = value
 
-with open('run.json', 'w') as file:
-    json.dump(data, file, indent=4)
+print(data)
+def replace_placeholders(data, params):
+    if isinstance(data, str):
+        for key, value in params.items():
+            data = data.replace(f"{key}", value)
+        return data
+    elif isinstance(data, dict):
+        return {key: replace_placeholders(value, params) for key, value in data.items()}
+    elif isinstance(data, list):
+        return [replace_placeholders(item, params) for item in data]
+    return data
+
+updated_json_data = replace_placeholders(data, params)    
+print(updated_json_data)
